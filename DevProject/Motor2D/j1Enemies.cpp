d91f13ce0d76c5b0cb321f729a6ceda2f5f1dd64
jose-tello <47975174+jose-tello@users.noreply.github.com>
2019-11-04 22:25:07 +0100

    module enemies added

---
+++ DevProject/Motor2D/j1Enemies.cpp
@@ -0,0 +1,150 @@
+#include "j1App.h"
+#include "j1Input.h"
+#include "j1Render.h"
+#include "j1Enemies.h"
+#include "j1Particles.h"
+#include "j1Textures.h"
+#include "j1Window.h"
+#include "p2Log.h"
+#include "Enemy.h"
+#include "Enemy_RedBird.h"
+
+#define SPAWN_MARGIN 50
+
+ModuleEnemies::ModuleEnemies()
+{
+	for (uint i = 0; i < MAX_ENEMIES; ++i)
+		enemies[i] = nullptr;
+}
+
+// Destructor
+ModuleEnemies::~ModuleEnemies()
+{
+}
+
+bool ModuleEnemies::Start()
+{
+	// Create a prototype for each enemy available so we can copy them around
+	//sprites = App->textures->Load("rtype/enemies.png");
+
+	return true;
+}
+
+bool ModuleEnemies::PreUpdate()
+{
+	// check camera position to decide what to spawn
+	for (uint i = 0; i < MAX_ENEMIES; ++i)
+	{
+		if (queue[i].type != ENEMY_TYPES::NO_TYPE)
+		{
+			if (queue[i].x * App->win->width < App->render->camera.x + (App->render->camera.w * App->win->scale) + SPAWN_MARGIN)
+			{
+				SpawnEnemy(queue[i]);
+				queue[i].type = ENEMY_TYPES::NO_TYPE;
+				LOG("Spawning enemy at %d", queue[i].x * App->win->scale);
+			}
+		}
+	}
+
+	return true;
+}
+
+// Called before render is available
+bool ModuleEnemies::Update(float dt)
+{
+	for (uint i = 0; i < MAX_ENEMIES; ++i)
+		if (enemies[i] != nullptr) enemies[i]->Move();
+
+	for (uint i = 0; i < MAX_ENEMIES; ++i)
+		if (enemies[i] != nullptr) enemies[i]->Draw(sprites);
+
+	return true;
+}
+
+bool ModuleEnemies::PostUpdate()
+{
+	// check camera position to decide what to spawn
+	for (uint i = 0; i < MAX_ENEMIES; ++i)
+	{
+		if (enemies[i] != nullptr)
+		{
+			if (enemies[i]->position.x * App->win->scale < (App->render->camera.x) - SPAWN_MARGIN)
+			{
+				LOG("DeSpawning enemy at %d", enemies[i]->position.x * App->win->scale);
+				delete enemies[i];
+				enemies[i] = nullptr;
+			}
+		}
+	}
+
+	return true;
+}
+
+// Called before quitting
+bool ModuleEnemies::CleanUp()
+{
+	LOG("Freeing all enemies");
+
+	App->tex->UnLoad(sprites);
+
+	for (uint i = 0; i < MAX_ENEMIES; ++i)
+	{
+		if (enemies[i] != nullptr)
+		{
+			delete enemies[i];
+			enemies[i] = nullptr;
+		}
+	}
+
+	return true;
+}
+
+bool ModuleEnemies::AddEnemy(ENEMY_TYPES type, int x, int y)
+{
+	bool ret = false;
+
+	for (uint i = 0; i < MAX_ENEMIES; ++i)
+	{
+		if (queue[i].type == ENEMY_TYPES::NO_TYPE)
+		{
+			queue[i].type = type;
+			queue[i].x = x;
+			queue[i].y = y;
+			ret = true;
+			break;
+		}
+	}
+
+	return ret;
+}
+
+void ModuleEnemies::SpawnEnemy(const EnemyInfo& info)
+{
+	// find room for the new enemy
+	uint i = 0;
+	for (; enemies[i] != nullptr && i < MAX_ENEMIES; ++i);
+
+	if (i != MAX_ENEMIES)
+	{
+		switch (info.type)
+		{
+		case ENEMY_TYPES::REDBIRD:
+			enemies[i] = new Enemy_RedBird(info.x, info.y);
+			break;
+		}
+	}
+}
+
+void ModuleEnemies::OnCollision(Collider* c1, Collider* c2)
+{
+	for (uint i = 0; i < MAX_ENEMIES; ++i)
+	{
+		if (enemies[i] != nullptr && enemies[i]->GetCollider() == c1)
+		{
+			enemies[i]->OnCollision(c2);
+			delete enemies[i];
+			enemies[i] = nullptr;
+			break;
+		}
+	}
+}
